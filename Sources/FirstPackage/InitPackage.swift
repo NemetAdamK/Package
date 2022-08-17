542e3f75046b4110a0dfeefed21b556fde4296d1
NemetAdamK <57504474+NemetAdamK@users.noreply.github.com>
2022-08-17 14:25:47 +0300

    Add files via upload

---
+++ Sources/FirstPackage/InitPackage.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ádám-Krisztián Német on 17.08.2022.
+//
+
+import Foundation
+
+public class InitPackage {
+    public func printLine() {
+        print("Heyyyyy")
+    }
+}
